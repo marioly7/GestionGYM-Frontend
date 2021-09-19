a4b832d7cb7bfe10ce9d29375575fac7fe31965b
Marioly <48572648+marioly7@users.noreply.github.com>
2021-09-19 02:09:07 -0400

    Formulario para registrar usuarios

--- app/src/main/java/com/example/gymmanagement/RegisterActivity.java
+++ app/src/main/java/com/example/gymmanagement/RegisterActivity.java
@@ -1,13 +1,54 @@
 package com.example.gymmanagement;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private Spinner spinner;
+    private static final String[] paths = {"Administrador", "Encargado", "Cliente"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        spinner = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(RegisterActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                Toast toast=Toast.makeText(getApplicationContext(),"Admi", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case 1:
+                Toast toast1=Toast.makeText(getApplicationContext(),"Encargado",Toast.LENGTH_SHORT);
+                toast1.show();
+                break;
+            case 2:
+                Toast toast2=Toast.makeText(getApplicationContext(),"Cliente",Toast.LENGTH_SHORT);
+                toast2.show();
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 }
